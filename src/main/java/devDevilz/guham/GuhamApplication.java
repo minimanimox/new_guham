2469353ae82a7b530b6f214013971203dc4cb8b7
minimanimox <purplemushroomsoup@gmail.com>
2022-05-12 16:47:56 +0900

    first commit

---
+++ src/main/java/devDevilz/guham/GuhamApplication.java
@@ -0,0 +1,13 @@
+package devDevilz.guham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuhamApplication.class, args);
+	}
+
+}
