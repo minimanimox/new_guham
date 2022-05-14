d1b3d5b869a6cf82b65a01c85f1da05b6c91c8b4
minimanimox <purplemushroomsoup@gmail.com>
2022-05-14 17:05:34 +0900

    css 추가

--- src/main/java/devDevilz/guham/config/WebSocketConfig.java
+++ src/main/java/devDevilz/guham/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
@@ -17,9 +18,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final static String CHAT_ENDPOINT = "/chat";
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(getChatWebSocketHandler,CHAT_ENDPOINT)
-                .setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+        /*
+         * withSockJS()
+         * 는 웹소켓을 지원하지 않는 브라우저에
+         * 폴백 옵션을 활성화하는데 사용됩니다.
+         * */
     }
 
     @Bean
