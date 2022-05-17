e772a8e7cda1d1b159e54375db2721b474753ccd
minimanimox <purplemushroomsoup@gmail.com>
2022-05-17 16:47:55 +0900

    Stomp drop 안하기로 하고 재 시도 / 테이블 연결 전

--- src/main/java/devDevilz/guham/entity/ChatRoom.java
+++ src/main/java/devDevilz/guham/entity/ChatRoom.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import java.util.UUID;
+
 
 @Getter
 @Setter
+@Entity
 @NoArgsConstructor
 public class ChatRoom {
 
